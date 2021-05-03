fd3616c9e0acab578fbb41edd892bdddd75098b6
ngxwinter <laila_daila@icloud.com>
2021-05-03 22:57:09 +0300

    Начальный коммит

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export type Config = {
+	readonly isProduction: boolean;
+};
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register';
+import * as fs from "fs";
+import * as rg from "rg";
+import { Config } from './config';
+
+async function main(): Promise<void> {
+	const RAW_DEFAULT_CONFIG = fs.readFileSync('./config.default.json', 'utf-8');
+	let RAW_CONFIG = "{}";
+
+	try {
+		RAW_CONFIG = fs.readFileSync("./config.json", "utf-8");
+	} catch (err) {
+		if (err.code !== "ENOENT") {
+			console.error(err);
+			process.exit(1);
+		}
+	}
+
+	const config = rg.getMergedObjects<Config>(
+		JSON.parse(RAW_DEFAULT_CONFIG),
+		JSON.parse(RAW_CONFIG)
+	);
+
+	if (config.isProduction) {
+		rg.setProduction();
+	}
+
+	console.log("Инициализация завершена!");
+}
+
+main();

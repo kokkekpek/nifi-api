3984726c16b08e62f3c3adcf5643f0d8bf69f539
ngxwinter <laila_daila@icloud.com>
2021-05-04 08:57:41 +0300

    Действия теперь сортируются по времени

--- src/actions/actions-manager.ts
+++ src/actions/actions-manager.ts
@@ -8,19 +8,34 @@ export class ActionsManager {
 		this.storage = storage;
 	}
 
+	private sortActionsByTime(action: Action[]): void {
+		action.sort((a, b) => {
+			return b.time - a.time;
+		});
+	}
+
 	public async addAction(action: Action): Promise<void> {
 		await this.storage.addAction(action);
 	}
 
 	public async getAllActions(): Promise<Action[]> {
-		return await this.storage.getAllActions();
+		const actions = await this.storage.getAllActions();
+		this.sortActionsByTime(actions);
+
+		return actions;
 	}
 
 	public async getActionsByTokenId(tokenId: string): Promise<Action[]> {
-		return await this.storage.getActionsByTokenId(tokenId);
+		const actions = await this.storage.getActionsByTokenId(tokenId);
+		this.sortActionsByTime(actions);
+
+		return actions;
 	}
 
 	public async getActionsByUserPublicKey(userPublicKey: string): Promise<Action[]> {
-		return await this.storage.getActionsByUserPublicKey(userPublicKey);
+		const actions = await this.storage.getActionsByUserPublicKey(userPublicKey);
+		this.sortActionsByTime(actions);
+
+		return actions;
 	}
 }

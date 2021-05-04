4b7a3a0baa7e1fa6ee84edb80c5d5bb0030184f6
ngxwinter <laila_daila@icloud.com>
2021-05-04 08:51:04 +0300

    Всем действиям добавлен публичный ключ пользователя

--- src/actions/actions-types.ts
+++ src/actions/actions-types.ts
@@ -15,6 +15,7 @@ export type ActionChangeOwner = BaseAction & {
 	readonly action: "changeOwner";
 	readonly tokenId: string;
 	readonly address: string;
+	readonly userPublicKey: string;
 	readonly previousOwner: string;
 	readonly owner: string;
 	readonly hash: string;
@@ -24,9 +25,10 @@ export type ActionSetHash = BaseAction & {
 	readonly action: "setHash";
 	readonly tokenId: string;
 	readonly address: string;
+	readonly userPublicKey: string;
 	readonly owner: string;
-	readonly hash: string;
 	readonly previousHash: string;
+	readonly hash: string;
 };
 
 export type Action = ActionCreateToken | ActionChangeOwner | ActionSetHash;

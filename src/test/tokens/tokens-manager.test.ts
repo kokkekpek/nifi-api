7181080ef66b2fb9f47729288069dc49cc9d9ef4
ngxwinter <laila_daila@icloud.com>
2021-05-05 07:32:55 +0300

    Тест токенов дополнен тестами обновления токенов

--- src/test/tokens/tokens-manager.test.ts
+++ src/test/tokens/tokens-manager.test.ts
@@ -103,3 +103,64 @@ test("Add tokens race condition avoid", async () => {
 		tokensManager.addToken(testToken1),
 	])).sort()).toMatchObject(["success", "token_with_such_id_already_exists"].sort(undefined));
 });
+
+test("Update token", async () => {
+	const testToken1: Token = {
+		id: "TestTokenId1",
+		address: "TestTokenAddress1",
+		userPublicKey: "TestTokenUserPublicKey1",
+		owner: "TestTokenOwner1",
+		hash: "TestTokenHash1"
+	};
+
+	await tokensManager.addToken(testToken1);
+
+	await tokensManager.setOwnerByTokenId(testToken1.id, "UPDATEDTestTokenOwner1");
+
+	expect((await tokensManager.getAllTokens())[0]?.owner).toBe("UPDATEDTestTokenOwner1");
+
+	await tokensManager.setHashByTokenId(testToken1.id, "UPDATEDTestTokenHash1");
+
+	const tokens = await tokensManager.getAllTokens();
+	expect(tokens[0]?.owner).toBe("UPDATEDTestTokenOwner1");
+	expect(tokens[0]?.hash).toBe("UPDATEDTestTokenHash1");
+
+	await tokensManager.setOwnerByTokenId(testToken1.id, "UPDATED2TestTokenOwner1");
+
+	const tokens2 = await tokensManager.getAllTokens();
+	expect(tokens2[0]?.owner).toBe("UPDATED2TestTokenOwner1");
+	expect(tokens2[0]?.hash).toBe("UPDATEDTestTokenHash1");
+
+	await tokensManager.setHashByTokenId(testToken1.id, "UPDATED2TestTokenHash1");
+
+	const tokens3 = await tokensManager.getAllTokens();
+	expect(tokens3[0]?.owner).toBe("UPDATED2TestTokenOwner1");
+	expect(tokens3[0]?.hash).toBe("UPDATED2TestTokenHash1");
+});
+
+test("Tokens update independently", async () => {
+	const testToken1: Token = {
+		id: "TestTokenId1",
+		address: "TestTokenAddress1",
+		userPublicKey: "TestTokenUserPublicKey1",
+		owner: "TestTokenOwner1",
+		hash: "TestTokenHash1"
+	};
+
+	const testToken2: Token = {
+		id: "TestTokenId2",
+		address: "TestTokenAddress2",
+		userPublicKey: "TestTokenUserPublicKey2",
+		owner: "TestTokenOwner2",
+		hash: "TestTokenHash2"
+	};
+
+	await tokensManager.addToken(testToken1);
+	await tokensManager.addToken(testToken2);
+
+	await tokensManager.setOwnerByTokenId(testToken1.id, "UPDATEDTestTokenOwner1");
+
+	const tokens = await tokensManager.getAllTokens();
+	expect(tokens[0]?.owner).toBe("UPDATEDTestTokenOwner1");
+	expect(tokens[1]?.owner).toBe("TestTokenOwner2");
+});

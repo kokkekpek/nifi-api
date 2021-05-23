0a3edc612728e8fa66cfc1b5e3a1b7848ca1a812
ngxwinter <laila_daila@icloud.com>
2021-05-23 12:49:31 +0300

    Адаптирован тест токенов под изменившиеся типы

--- src/test/tokens/tokens-manager.test.ts
+++ src/test/tokens/tokens-manager.test.ts
@@ -2,8 +2,43 @@ import { createConnection, getConnection } from "typeorm";
 import { Token, TokensManager } from "../../tokens/tokens-manager";
 import { TokensStorageDatabase } from "../../tokens/tokens-storage-database";
 import { DatabaseToken } from "../../database/models/token";
+import { AuctionStorageEntry, IAuctionsStorage } from "../../auctions/auctions-storage";
+import { IBidsStorage } from "../../auctions/bids-storage";
+import { AuctionBid, AuctionsManager } from "../../auctions/auctions-manager";
 
 let tokensManager: TokensManager;
+
+class MockAuctionsStorage implements IAuctionsStorage {
+	public async getAuctionByAuctionId(auctionId: string): Promise<AuctionStorageEntry | undefined> {
+		return;
+	}
+
+	public async hasAuctionWithAuctionId(auctionId: string): Promise<boolean> {
+		return false;
+	}
+
+	public async addAuction(auction: AuctionStorageEntry): Promise<void> {
+		// PASS
+	}
+
+	public async setAuctionFinishBid(auctionId: string, finishBid: string): Promise<void> {
+		// PASS
+	}
+}
+
+class MockBidsStorage implements IBidsStorage {
+	public async addBid(bid: AuctionBid): Promise<void> {
+		// PASS
+	}
+
+	public async getBidsByAuctionId(auctionId: string): Promise<AuctionBid[]> {
+		return [];
+	}
+
+	public async getBidByBidId(bidId: string): Promise<AuctionBid | undefined> {
+		return;
+	}
+}
 
 beforeEach(async () => {
 	const connection = await createConnection({
@@ -15,8 +50,13 @@ beforeEach(async () => {
 		logging: false
 	});
 
+	const auctionsManager = new AuctionsManager(
+		new MockAuctionsStorage(),
+		new MockBidsStorage()
+	);
+
 	const storage = new TokensStorageDatabase(connection.getRepository(DatabaseToken));
-	tokensManager = new TokensManager(storage);
+	tokensManager = new TokensManager(storage, auctionsManager);
 });
 
 afterEach(() => {
@@ -32,7 +72,8 @@ test("Get and add tokens", async () => {
 		address: "TestTokenAddress1",
 		userPublicKey: "TestTokenUserPublicKey1",
 		owner: "TestTokenOwner1",
-		hash: "TestTokenHash1"
+		hash: "TestTokenHash1",
+		auction: null
 	};
 
 	expect(await tokensManager.addToken(testToken1)).toBe("success");
@@ -57,7 +98,8 @@ test("Get and add tokens", async () => {
 		address: "TestTokenAddress2",
 		userPublicKey: "TestTokenUserPublicKey2",
 		owner: "TestTokenOwner2",
-		hash: "TestTokenHash2"
+		hash: "TestTokenHash2",
+		auction: null
 	};
 
 	expect(await tokensManager.addToken(testToken2)).toBe("success");
@@ -94,7 +136,8 @@ test("Add two tokens with the same id", async () => {
 		address: "TestTokenAddress1",
 		userPublicKey: "TestTokenUserPublicKey1",
 		owner: "TestTokenOwner1",
-		hash: "TestTokenHash1"
+		hash: "TestTokenHash1",
+		auction: null
 	};
 
 	expect(await tokensManager.addToken(testToken1)).toBe("success");
@@ -107,7 +150,8 @@ test("Add tokens race condition avoid", async () => {
 		address: "TestTokenAddress1",
 		userPublicKey: "TestTokenUserPublicKey1",
 		owner: "TestTokenOwner1",
-		hash: "TestTokenHash1"
+		hash: "TestTokenHash1",
+		auction: null
 	};
 
 	expect((await Promise.all([
@@ -122,7 +166,8 @@ test("Update token", async () => {
 		address: "TestTokenAddress1",
 		userPublicKey: "TestTokenUserPublicKey1",
 		owner: "TestTokenOwner1",
-		hash: "TestTokenHash1"
+		hash: "TestTokenHash1",
+		auction: null
 	};
 
 	await tokensManager.addToken(testToken1);
@@ -156,7 +201,8 @@ test("Tokens update independently", async () => {
 		address: "TestTokenAddress1",
 		userPublicKey: "TestTokenUserPublicKey1",
 		owner: "TestTokenOwner1",
-		hash: "TestTokenHash1"
+		hash: "TestTokenHash1",
+		auction: null
 	};
 
 	const testToken2: Token = {
@@ -164,7 +210,8 @@ test("Tokens update independently", async () => {
 		address: "TestTokenAddress2",
 		userPublicKey: "TestTokenUserPublicKey2",
 		owner: "TestTokenOwner2",
-		hash: "TestTokenHash2"
+		hash: "TestTokenHash2",
+		auction: null
 	};
 
 	await tokensManager.addToken(testToken1);
